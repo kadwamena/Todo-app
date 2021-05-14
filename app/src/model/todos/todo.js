cbf7b129a6d4bceb0ab1ea4f6cacc00424b2609b
Anah Victor <30688175+anahvictoronyedic@users.noreply.github.com>
2021-05-14 17:41:10 +0100

    Documentation done

--- app/src/model/todos/todo.js
+++ app/src/model/todos/todo.js
@@ -40,58 +40,91 @@ if (config.usefake) {
 
 module.exports = {
 
-  //Add todo function
+  // adds new todo to the storage
   addTodo: function (todo) {
     const id = Date.now();
 
-    // 
+    // assign the new todo anid
     todo.id = id;
 
-    //
+    // push into storage array
     todostore.push(todo);
 
+    // return the effective todo, which contains the id
     return todo;
   },
 
-  //
+  // deletes todo from data storage
   deleteTodo: function (id) {
+    
+    // create a temporal store
     const tempStore = [];
 
-    //
+    // for each todo in todostore
     for (const todo of todostore) {
+      
+      // if not todo to be deleted then push, else skip
       if (todo.id != id) {
+        
+        // put in temporal store
         tempStore.push(todo);
       }
     }
 
+    // assign todos that were not skipped as current storage items
     todostore = tempStore;
   },
+  
+  // edits a todo provided its id. Also accepts the new todo that should replace the current in storage
   editTodo: function (id, todo) {
+    
+    // create a temporal store
     const tempStore = [];
 
+    // optional. but makes sure id property is present
     todo.id = id;
 
+    // for each todo in todostore
     for (const todoitem of todostore) {
+      
+      // if id that should be updated
       if (todoitem.id == id) {
+        
+        // push the new todo
         tempStore.push(todo);
-      } else tempStore.push(todoitem);
+      } 
+      
+      // push the current todo
+      else tempStore.push(todoitem);
     }
 
+    // update our storage
     todostore = tempStore;
 
+    // return what was saved
     return todo;
   },
 
+  // gets a particular todo, given its id
   getTodo: function (id) {
+    
+    // for each todo in todostore
     for (const todo of todostore) {
+      
+      // if its the todo we are looking for the return it
       if (todo.id == id) {
         return todo;
       }
     }
+    
+    // return empty if not found
     return null;
   },
 
+  // get all todos
   getTodos: function (todo) {
+    
+    // we clone to return a copy of the storage,to avoid direct manipulation
     return todostore.slice(0);
   },
 };
